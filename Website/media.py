00e259df2b891167c709c2419c7563a9160b36bd
Shivang Medheakar <shivangmedhekar@gmail.com>
2024-09-30 16:42:04 -0400

    Update media.py
    add comments

--- Website/media.py
+++ Website/media.py
@@ -1,11 +1,22 @@
 # Defining the Movie class
 class Movie():
+    """
+    A class to represent a movie with its details.
+    
+    Attributes:
+        title (str): The title of the movie.
+        storyline (str): A brief description of the movie's plot.
+        genre (str): The genre of the movie.
+        protagonist (str): The main actor or protagonist of the movie.
+        director (str): The director of the movie.
+        year (str): The release year of the movie.
+        poster_image_url (str): URL to the movie's poster image.
+        trailer_youtube_url (str): URL to the movie's trailer on YouTube.
+    """
 
     # Class variable to count the number of instances created
     TOTAL_INSTANCES = 0
 
-    # Defining the constructor, called whenever
-    # a new instace of the class is created.
     def __init__(
                 self,
                 movie_title,
@@ -16,6 +27,19 @@ class Movie():
                 movie_year,
                 poster_image,
                 trailer_youtube):
+        """
+        The constructor for the Movie class.
+        
+        Args:
+            movie_title (str): The title of the movie.
+            movie_storyline (str): A brief description of the movie's plot.
+            movie_genre (str): The genre of the movie.
+            movie_protagonist (str): The main actor or protagonist of the movie.
+            movie_director (str): The director of the movie.
+            movie_year (str): The release year of the movie.
+            poster_image (str): URL to the movie's poster image.
+            trailer_youtube (str): URL to the movie's trailer on YouTube.
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.genre = movie_genre
@@ -24,4 +48,4 @@ class Movie():
         self.year = movie_year
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-        Movie.TOTAL_INSTANCES += 1
+        Movie.TOTAL_INSTANCES += 1  # Increment the class variable for each new instance
